ab1035ee1a9c2ef2503f3d6948a75e6e68590b73
A7K4M <cy9i8e@gmail.com>
2024-08-06 15:24:10 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -28,7 +28,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 from helper.database import db
 from config import Config, Txt  
 from utils import check_verification, get_token, verify_user, check_token
-from config import VERIFY, VERIFY_TUTORIAL, BOT_USERNAME
+from info import VERIFY, VERIFY_TUTORIAL, BOT_USERNAME
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
@@ -36,11 +36,11 @@ async def start(client, message):
     await db.add_user(client, message)
 
     # Check verification status
-    if not await check_verification(client, user.id) and Config.VERIFY:
+    if not await check_verification(client, user.id) and VERIFY:
         btn = [[
             InlineKeyboardButton("Verify", url=await get_token(client, user.id, f"https://telegram.me/{Config.BOT_USERNAME}?start="))
         ],[
-            InlineKeyboardButton("How To Open Link & Verify", url=Config.VERIFY_TUTORIAL)
+            InlineKeyboardButton("How To Open Link & Verify", url=VERIFY_TUTORIAL)
         ]]
         await message.reply_text(
             text="<b>You are not verified!\nKindly verify to continue!</b>",
